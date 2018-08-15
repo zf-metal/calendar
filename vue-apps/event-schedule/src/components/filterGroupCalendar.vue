dbfe9c8983d7fedde0f7617676d929dd7c524520
crist <cristian.cdi@gmail.com>
2018-08-15 08:02:09 -0300

    asd

---
+++ vue-apps/event-schedule/src/components/filterGroupCalendar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello">
+    </div>
+</template>
+
+<script>
+    import {mapGetters, mapActions} from 'vuex';
+
+    export default {
+        name: 'hello',
+        props:[],
+        components: {},
+        data() {
+            return {
+                asd: {}
+            }
+        },
+        created: function () {
+        },
+        methods: {},
+        computed: {
+            ...mapGetters([
+                'getLoading'
+            ]),
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
