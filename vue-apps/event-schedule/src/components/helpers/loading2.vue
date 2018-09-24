b2f8421ff8f26bd1875530591028c212197c48a1
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-09-24 13:07:39 -0300

    Fix loading bar

--- vue-apps/event-schedule/src/components/helpers/loading2.vue
+++ vue-apps/event-schedule/src/components/helpers/loading2.vue
@@ -1,9 +1,4 @@
 <template>
-    <!--<div class="load-bar" v-if="isLoading">-->
-        <!--<div class="bar"></div>-->
-        <!--<div class="bar"></div>-->
-        <!--<div class="bar"></div>-->
-    <!--</div>-->
     <div class="load-bar" v-if="isLoading">
         <v-progress-linear :indeterminate="true" class="pa-0 ma-0"></v-progress-linear>
     </div>
@@ -55,47 +50,5 @@
         /*background-color: #fdba2c;*/
         z-index: 3;
 
-    }
-
-    .bar {
-        content: "";
-        display: inline;
-        position: absolute;
-        width: 0;
-        height: 100%;
-        left: 50%;
-        text-align: center;
-    }
-
-    .bar:nth-child(1) {
-        background-color: #da4733;
-        animation: loading 3s linear infinite;
-    }
-
-    .bar:nth-child(2) {
-        background-color: #3b78e7;
-        animation: loading 3s linear 1s infinite;
-    }
-
-    .bar:nth-child(3) {
-        background-color: #fdba2c;
-        animation: loading 3s linear 2s infinite;
-    }
-
-    @keyframes loading {
-        from {
-            left: 50%;
-            width: 0;
-            z-index: 100;
-        }
-        33.3333% {
-            left: 0;
-            width: 100%;
-            z-index: 10;
-        }
-        to {
-            left: 0;
-            width: 100%;
-        }
     }
 </style>
