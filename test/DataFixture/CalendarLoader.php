c6c93eaafac8113438b31bf36df83ce49b4fba2e
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2019-02-04 12:13:49 -0300

    Se inicia Test para PredefinedEvents

---
+++ test/DataFixture/CalendarLoader.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Test\DataFixture;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\ORM\EntityManager;
+use ZfMetal\Calendar\Entity\Calendar;
+
+class CalendarLoader extends AbstractFixture implements FixtureInterface
+{
+
+    const ENTITY = Calendar::class;
+
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $calendars;
+
+    /**
+     * @return mixed
+     */
+    public function getEm()
+    {
+        return $this->em;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCalendars()
+    {
+        if (!$this->calendars) {
+            $this->calendars = new ArrayCollection();
+        }
+        return $this->calendars;
+    }
+
+
+    protected function findByName($name)
+    {
+        return $this->getEm()->getRepository($this::ENTITY)->findOneByName($name);
+    }
+
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $this->em = $manager;
+
+        $this->createCalendar(1, "CalendarTest", 'schedule1');
+        $manager->flush();
+
+
+    }
+
+
+    public function createCalendar($id, $name,$schedule)
+    {
+
+        $calendar = $this->findByName($name);
+        if (!$calendar) {
+            $calendar = new Calendar();
+            $calendar->setId($id);
+            $calendar->setName($name);
+         //   $calendar->addSchedule($this->getReference($schedule));
+
+        }
+
+        $this->getEm()->persist($calendar);
+
+        //Add reference for relations
+        $this->addReference($name, $calendar);
+
+        $this->getCalendars()->add($calendar);
+    }
+
+}
---
+++ test/DataFixture/ScheduleLoader.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Test\DataFixture;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\ORM\EntityManager;
+use ZfMetal\Calendar\Entity\Schedule;
+
+class ScheduleLoader extends AbstractFixture implements FixtureInterface
+{
+
+    const ENTITY = Schedule::class;
+
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $schedules;
+
+    /**
+     * @return mixed
+     */
+    public function getEm()
+    {
+        return $this->em;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getSchedules()
+    {
+        if (!$this->schedules) {
+            $this->schedules = new ArrayCollection();
+        }
+        return $this->schedules;
+    }
+
+
+    protected function find($id)
+    {
+        return $this->getEm()->getRepository($this::ENTITY)->find($id);
+    }
+
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $this->em = $manager;
+
+        $this->createSchedule(1, "CalendarTest");
+        $manager->flush();
+
+
+    }
+
+
+    public function createSchedule($id, $calendar)
+    {
+
+        $schedule = $this->find($id);
+        if (!$schedule) {
+            $schedule = new Schedule();
+            $schedule->setId($id);
+            $schedule->setCalendar($this->getReference($calendar));
+            $schedule->setDay(1);
+            $schedule->setStart(\DateTime::createFromFormat('H:i','09:00'));
+            $schedule->setEnd(\DateTime::createFromFormat('H:i','12:00'));
+        }
+
+        $this->getEm()->persist($schedule);
+
+        //Add reference for relations
+        $this->addReference('schedule'.$id, $schedule);
+
+        $this->getSchedules()->add($schedule);
+    }
+
+}
